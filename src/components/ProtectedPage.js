b50110a8fd9e5fa85cc78ae090f6bf198983a550
alex-png <alexrod102797@gmail.com>
2019-12-19 15:25:17 -0500

    working

---
+++ src/components/ProtectedPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+class ProtectedPage extends React.Component {
+  render() {
+    return <h1>You're inside the protected page!</h1>
+  }
+}
+ 
+export default ProtectedPage;
